55c1eb6a865911c3ac052330956c713c044615af
Anbunithi <140570489+anbunithi-dev@users.noreply.github.com>
2023-12-26 23:00:27 +0530

    Update execute.sh

--- DevOps_Project_1/execute.sh
+++ DevOps_Project_1/execute.sh
@@ -8,15 +8,15 @@ export AWS_PROFILE="Anbunithi"
 echo $AWS_PROFILE
 
 whoami
-aws ecr get-login-password  --region us-east-1 | sudo docker login --username AWS --password-stdin "476224453880.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password  --region us-east-1 | sudo docker login --username AWS --password-stdin "476224453880.dkr.ecr.us-east-1.amazonaws.com"
 
 sudo docker build -t nginx-application .
 
-sudo docker tag nginx-application:latest 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION
+sudo docker tag nginx-application:latest 476224453880.dkr.ecr.us-east-1.amazonaws.com/nginx-application:$VERSION
 
-sudo docker push 044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION
+sudo docker push 476224453880.dkr.ecr.us-east-1.amazonaws.com/nginx-application:$VERSION
 
-DOCKER_IMAGE="044967670847.dkr.ecr.ap-south-1.amazonaws.com/nginx-application:$VERSION"
+DOCKER_IMAGE="476224453880.dkr.ecr.us-east-1.amazonaws.com/nginx-application:$VERSION"
 
 sed -i "s@devops_image@$DOCKER_IMAGE@g" nginx_deployment.yaml
 
